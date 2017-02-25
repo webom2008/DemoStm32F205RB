8b8b4cf927b7d4a3a5a4fd62f7a13ccf29eef6fc
qiuweibo <qiuweibo@cvte.com>
2017-02-25 17:10:27 +0800

    [feature] add tools for arm-gcc
    [what]
    [why]
    [how]

--- domake.sh
+++ domake.sh
@@ -2,7 +2,7 @@
 
 
 # export tool-chain env
-#source ./env.rc
+source ./env.rc
 
 
 make clean
---
+++ tools/tar.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+TARGET=gcc-arm-none-eabi-5_4-2016q3
+#TARGET=arm-2011.03
+
+#tar
+#tar -czf $TARGET.tar.gz $TARGET
+#split -b 80M -d -a 1 $TARGET.tar.gz $TARGET.tar.gz.
+
+#untar
+cat $TARGET.tar.gz.* | tar -zxv
+
+rm $TARGET.tar.gz
+
